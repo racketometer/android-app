15c09c77046bb9d1f9c4cac8861c41d0a1ca975e
Bjørn Sørensen <bjoern.soerensen@gmail.com>
2016-05-17 09:56:00 +0200

    Add Android Studio Project.

---
+++ app/src/test/java/com/smap/f16/grp12/racketometer/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.smap.f16.grp12.racketometer;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
